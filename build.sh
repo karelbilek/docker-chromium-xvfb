a16f9665dc3b29b46c7a813ffff0bb51fd51ef0d
Karel Bilek <karel.bilek@brank.as>
2021-05-08 02:47:39 +0700

    shfmt

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ docker build -t docker-chromium-xvfb .
 
 #get the chromium version from inside the docker
 
-LONGVERSION=`docker run docker-chromium-xvfb /usr/bin/chromium --version`
+LONGVERSION=$(docker run docker-chromium-xvfb /usr/bin/chromium --version)
 #looks something like 'Chromium 90.0.4430.93 built on Debian 10.9, running on Debian 10.9'
 
-SHORTVERSION=`echo $LONGVERSION | sed 's/Chromium \(.*\) built .*/\1/'`
+SHORTVERSION=$(echo $LONGVERSION | sed 's/Chromium \(.*\) built .*/\1/')
 
 docker tag docker-chromium-xvfb karel3d/docker-chromium-xvfb:latest
 docker push karel3d/docker-chromium-xvfb:latest
@@ -24,4 +24,3 @@ docker push karel3d/chromium-xvfb-plus-headless:latest
 
 docker tag chromium-xvfb-plus-headless karel3d/chromium-xvfb-plus-headless:$SHORTVERSION
 docker push karel3d/chromium-xvfb-plus-headless:$SHORTVERSION
-
